e3a339d80c52cd542ed2c3cde4e9e89e6a43cbb9
Jonathan Beliën <jbe@geo6.be>
2017-10-19 16:07:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Itinero Routing
+
+The goal of this tool is to calculate routes between 2 datasets of points with *Routing API* from *Itinero* (<https://github.com/itinero/routing-api>).  
+*Itinero Routing API* uses the data and geometry from [*OpenStreetMap*](https://openstreetmap.org/) !
+
+## Install
+
+The tool only requires **PHP 7.0+**.
+
+```
+git clone https://github.com/geo6/itinero-routing
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+
+# Install dependencies
+php composer.phar install
+```
+
+## Usage
+
+```
+php validate.php --from=data/testFrom.csv --to=data/testTo.csv --api=http://localhost/belgium/
+```
+
+### Options
+
+| Option      | Default   | Description                                                                  |
+|-------------|-----------|------------------------------------------------------------------------------|
+| **--from**  | -         | **Required** - CSV file containing **from** points                           |
+| **--to**    | -         | **Required** - CSV file containing **to** points                             |
+| **--api**   | -         | **Required** - URL to *Itinero Routing API*                                  |
+| --profile   | `car`     | Routing profile used by *Itinero Routing API*                                |
+| --mode      | `fastest` | Minimize time with `fastest` value, or distance with `closest` value         |
+| --no-header | -         | Add this option if your CSV file does not contain columns name in first line |
---
+++ route.php
@@ -0,0 +1,118 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+
+$options = getopt('', array('from:', 'to:', 'api:', 'profile::', 'mode::', 'no-header'));
+
+$profile = (isset($options['profile']) ? $options['profile'] : 'car');
+$mode = (isset($options['mode']) && in_array($options['mode'], array('closest', 'fastest')) ? $options['mode'] : 'fastest');
+$header = (isset($options['no-header']) ? !($options['no-header']) : TRUE);
+$fileFrom = $options['from'];
+$fileTo = $options['to'];
+$api = $options['api'];
+
+$cursorFrom = 0;
+$cursorTo = 0;
+
+echo '--------------------------------------------------'.PHP_EOL;
+echo 'Profile : '.$profile.PHP_EOL;
+echo 'Mode    : '.$mode.PHP_EOL;
+echo '--------------------------------------------------'.PHP_EOL;
+
+$client = new Client(['base_uri' => $api]);
+
+if (isset($fileFrom) && file_exists($fileFrom)) {
+  $fname = pathinfo($fileFrom, PATHINFO_FILENAME);
+  $dir = 'data/'.$fname;
+
+  if (!file_exists($dir) || !is_dir($dir)) {
+    mkdir($dir);
+  }
+
+  if (($handleFrom = fopen($fileFrom, 'r')) !== FALSE && ($handleTo = fopen($fileTo, 'r')) !== FALSE) {
+    $fpResultFull = fopen($dir.'/result-full.csv', 'w');
+    $fpResult = fopen($dir.'/result.csv', 'w');
+
+    while (($dataFrom = fgetcsv($handleFrom, 1000)) !== FALSE) {
+      if ($header === TRUE && $cursorFrom === 0) {
+        $cursorFrom++;
+        continue;
+      }
+
+      $min = NULL;
+      $minTo = NULL;
+      $minResult = NULL;
+
+      while (($dataTo = fgetcsv($handleTo, 1000)) !== FALSE) {
+        if ($header === TRUE && $cursorTo === 0) {
+          $cursorTo++;
+          continue;
+        }
+
+        try {
+          $url = sprintf('routing?profile=%s&loc=%f,%f&loc=%f,%f&sort=true', $profile, $dataFrom[1], $dataFrom[0], $dataTo[1], $dataTo[0]);
+          //echo $cursorFrom.'   ||   '.$cursorTo.'   ||   '.$url.PHP_EOL;
+          $response = $client->get($url, [
+              'headers' => [
+                'Accept' => 'application/json',
+                'User-Agent' => 'PHP/'.phpversion()
+              ]
+          ]);
+          $json = json_decode((string)$response->getBody());
+
+          $last = end($json->features);
+
+          $result = array_merge(
+            $dataFrom,
+            $dataTo,
+            array($mode, $last->properties->time, $last->properties->distance)
+          );
+          fputcsv($fpResultFull, $result);
+
+          if ($mode === 'closest' && (is_null($min) || $last->properties->distance < $min)) {
+            $min = $last->properties->distance;
+            $minTo = $dataTo;
+            $minResult = array($last->properties->time, $last->properties->distance);
+          }
+          else if ($mode === 'fastest' && (is_null($min) || $last->properties->time < $min)) {
+            $min = $last->properties->time;
+            $minTo = $dataTo;
+            $minResult = array($last->properties->time, $last->properties->distance);
+          }
+        } catch (ClientException $e) {
+          $request_error = Psr7\str($e->getResponse()); trigger_error($request_error, E_USER_ERROR);
+        } catch (ServerException $e) {
+          $request_error = Psr7\str($e->getResponse()); trigger_error($request_error, E_USER_ERROR);
+        } catch (Exception $e) {
+          $request_error = $e->getMessage(); trigger_error($request_error, E_USER_ERROR);
+        }
+
+        $cursorTo++;
+      }
+
+      echo $cursorFrom.'   ||   '.$dataFrom[2].'   ||   '.$minTo[2].'   ||   Time: '.$minResult[0].'  -  Distance: '.$minResult[1].PHP_EOL;
+
+      $result = array_merge(
+        $dataFrom,
+        $minTo,
+        array($mode),
+        $minResult
+      );
+      fputcsv($fpResult, $result);
+
+      rewind($handleTo);
+      $cursorTo = 0;
+
+      $cursorFrom++;
+    }
+
+    fclose($fpResultFull);
+    fclose($fpResult);
+  }
+}
+
+exit();
